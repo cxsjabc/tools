80f2de524f3a0b80be10ee05388b58fe46545e72
Xi Chen <511272827@qq.com>
2017-06-17 18:10:16 +0800

    optimize github.sh: 	can input: github.sh algo  or github.sh alg to open githum algorithm repo webpage.
    Signed-off-by: Xi Chen <511272827@qq.com>

--- github.sh
+++ github.sh
@@ -3,8 +3,19 @@
 
 while [ ! -z $1 ]
 do
-	echo "Will open github :$1"
-	open https://github.com/cxsjabc/$1
+	# special case for simple algorithm repo accessing
+	REPO=${1:0:2}
+	if [ $REPO == 'al' ]
+	then 
+		REPO=algorithm
+	else
+		REPO=$1
+	fi
+	echo "Will open github :$REPO"
+
+	# because i am running on Mac, so i can use "open" command.
+	# if linux or windows, please use commands themselves.	
+	open https://github.com/cxsjabc/$REPO
 	shift
 
 	# sleep 5
