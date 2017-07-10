05cd1a84dd109f90123cd4b731935b09bc2b279f
Xi Chen <511272827@qq.com>
2017-07-10 22:59:02 +0800

    common_non_sys.sh
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ common_non_sys.sh
@@ -0,0 +1,5 @@
+
+#REL_PATH=../
+
+#COMMON_C="${REL_PATH}common/common.c ${REL_PATH}common/gen.c ${REL_PATH}common/arr.c ${REL_PATH}common/matrix.c"
+COMMON_C="${REL_PATH}common/common_all_in_one_non_sys.c"
