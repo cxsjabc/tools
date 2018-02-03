328033b8a04954e1551fbc555d110734a8cb4c39
Xi Chen <511272827@qq.com>
2018-02-03 21:47:47 +0800

    fix common.sh path postfix '\'
    Signed-off-by: Xi Chen <511272827@qq.com>

--- common.sh
+++ common.sh
@@ -2,4 +2,4 @@
 #REL_PATH=../
 
 #COMMON_C="${REL_PATH}common/common.c ${REL_PATH}common/gen.c ${REL_PATH}common/arr.c ${REL_PATH}common/matrix.c"
-COMMON_C="${REL_PATH}common/common_all_in_one.c"
+COMMON_C="${REL_PATH}/common/common_all_in_one.c"
