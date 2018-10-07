919aabd2ad901b4aaa69dbac19fc39d5ce182d7f
Xi Chen <xichen@511272827.com>
2018-10-07 09:42:35 +0800

    args
    Signed-off-by: Xi Chen <xichen@511272827.com>

---
+++ parse_args.c
@@ -0,0 +1,11 @@
+/*
+* Copyright by xichen(Xi.chen)  511272827@qq.com
+*/
+#include "common_local.h"
+
+int main(int argc, char *argv[])
+{
+    if(argc > 1)
+	    printf("argv[1]: %s\n", argv[1]);	
+    return 0;
+}
