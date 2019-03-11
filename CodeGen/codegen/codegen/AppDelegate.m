0eecb4a3d25954ce4c21b20660595fd109683420
Xi Chen <511272827@qq.com>
2019-03-11 22:29:54 +0800

    CodeGen first version
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ CodeGen/codegen/codegen/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  codegen1
+//
+//  Created by eyu on 2019/3/10.
+//  Copyright © 2019年 eyu. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
